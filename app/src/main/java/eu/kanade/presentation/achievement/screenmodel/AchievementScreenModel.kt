b02db9cadf12382a7495608f78118fb70b3be932
andreykolesnikov <andreykolesnikov96@gmail.com>
2026-06-29 21:34:14 +0500

    [achievement] Fix Time Paradox filtering on Secret tab

--- app/src/main/java/eu/kanade/presentation/achievement/screenmodel/AchievementScreenModel.kt
+++ app/src/main/java/eu/kanade/presentation/achievement/screenmodel/AchievementScreenModel.kt
@@ -167,7 +167,9 @@ sealed interface AchievementScreenState {
                     it.category == AchievementCategory.NOVEL ||
                         it.category == AchievementCategory.BOTH
                 }
-                AchievementCategory.SECRET -> achievements.filter { it.category == AchievementCategory.SECRET }
+                AchievementCategory.SECRET -> achievements.filter {
+                    it.category == AchievementCategory.SECRET || it.isSecret
+                }
             }
 
         val totalPoints: Int
